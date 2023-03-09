97bc0be435b369cfc52f4bc25c0c806987f095b7
Jon Minton <jon.will.minton@gmail.com>
2023-03-09 14:30:52 +0000

    Complete second tdd (game results)

--- wk_02/dy_4/tdd_lab/start_code/football_results/src/football_results.py
+++ wk_02/dy_4/tdd_lab/start_code/football_results/src/football_results.py
@@ -1,6 +1,11 @@
 def get_result(final_score):
-    pass
+    if final_score["home_score"] > final_score["away_score"]:
+        return "Home win"
+    if final_score["home_score"] < final_score["away_score"]:
+        return "Away win"
+    if final_score["home_score"] == final_score["away_score"]:
+        return "Draw"
 
 def get_results(final_scores):
-    pass
+    return [get_result(x) for x in final_scores]
     # (You could try and use a list comprehension for this)
--- wk_02/dy_4/tdd_lab/start_code/football_results/tests/football_results_test.py
+++ wk_02/dy_4/tdd_lab/start_code/football_results/tests/football_results_test.py
@@ -2,15 +2,37 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.game1 = {"home_score" : 0, "away_score": 3}
+        self.game2 = {"home_score" : 0, "away_score": 0}
+        self.game3 = {"home_score" : 2, "away_score": 1}
+        self.games = [self.game1, self.game2, self.game3]
+
     # Test we get the right result string for a final score dictionary representing -
+    def test_returns_which_team_wins(self):
+        self.assertEqual(
+            "Home win", 
+            get_result(self.game3)
+        )
+        self.assertEqual(
+            "Away win",
+            get_result(self.game1)
+        )
+        self.assertEqual(
+            "Draw",
+            get_result(self.game2)
+        )
 
         # Home win
         # Away win
         # Draw
 
     # Test we get right list of result strings for a list of final score dictionaries. 
-
+    def test_list_of_game_results_is_correct(self):
+        self.assertEqual(
+            ["Away win", "Draw", "Home win"],
+            get_results(self.games)
+        )
 
 if __name__ == "__main__":
     unittest.main()
