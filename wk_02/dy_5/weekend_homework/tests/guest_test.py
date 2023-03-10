1263d2db085570c9881f1794acaa86368ed65f3e
Jon Minton <jon.will.minton@gmail.com>
2023-03-10 16:26:46 +0000

    make test names more sensible

--- wk_02/dy_5/weekend_homework/tests/guest_test.py
+++ wk_02/dy_5/weekend_homework/tests/guest_test.py
@@ -8,14 +8,14 @@ class TestGuest(unittest.TestCase):
         self.guest2 = Guest("Stevi", 200, "Dreams")
 
 
-    def test_guests_have_name(self):
+    def test_has_name(self):
         self.assertEqual("Steve", self.guest1.name)
         self.assertEqual("Stevi", self.guest2.name)
 
-    def test_guests_have_wallet(self):
+    def test_has_wallet(self):
         self.assertEqual(50, self.guest1.wallet)
         self.assertEqual(200, self.guest2.wallet)
 
-    def test_guests_have_favourite_song(self):
+    def test_has_favourite_song(self):
         self.assertEqual("Copperhead Road", self.guest1.favourite_song)
         self.assertEqual("Dreams", self.guest2.favourite_song)
