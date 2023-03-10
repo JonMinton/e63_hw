ad9b70bb741422fd6a7e9d473e9e35d09984a9a3
Jon Minton <jon.will.minton@gmail.com>
2023-03-10 10:32:25 +0000

    complete main compound interest tests

--- wk_02/dy_4/tdd_lab/start_code/compound_interest/src/compound_interest.py
+++ wk_02/dy_4/tdd_lab/start_code/compound_interest/src/compound_interest.py
@@ -1,2 +1,14 @@
 class CompoundInterest:
-    pass
+    def __init__(self, P, r, t, n):
+        self.P = P
+        self.r = r
+        self.t = t
+        self.n = n
+
+    def calc_end_amount(self):
+        self.A = self.P * (1 + self.r / self.n) ** (self.n * self.t)
+
+    def return_end_amount(self):
+        return self.A
+
+# A = P(1 + r/n)<sup>nt</sup>
--- wk_02/dy_4/tdd_lab/start_code/compound_interest/tests/compound_interest_test.py
+++ wk_02/dy_4/tdd_lab/start_code/compound_interest/tests/compound_interest_test.py
@@ -7,14 +7,33 @@ class CompoundInterestTest(unittest.TestCase):
     # Tests
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
-
+    def test_scenario_01(self):
+        calc_01 = CompoundInterest(100, 0.10, 20, 12)
+        calc_01.calc_end_amount()
+        self.assertEqual(732.81, round(calc_01.return_end_amount(), 2))
     # Should return 181.94 given 100 principal, 6 percent, 10 years
+    def test_scenario_02(self):
+        calc_02 = CompoundInterest(100, 0.06, 10, 12)
+        calc_02.calc_end_amount()
+        self.assertEqual(181.94, round(calc_02.return_end_amount(), 2))
 
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
+    def test_scenario_03(self):
+        calc_03 = CompoundInterest(100000, 0.05, 8, 12)
+        calc_03.calc_end_amount()
+        self.assertEqual(149058.55, round(calc_03.return_end_amount(), 2))
 
     # Should return 0.00 given 0 principal, 10 percent, 1 year
+    def test_scenario_04(self):
+        calc_04 = CompoundInterest(0, 0.10, 1, 12)
+        calc_04.calc_end_amount()
+        self.assertEqual(0.00, round(calc_04.return_end_amount(), 2))
 
     # Should return 100.00 given 100 principal, 0 percent, 10 years
+    def test_scenario_05(self):
+        calc_05 = CompoundInterest(100, 0.00, 10, 12)
+        calc_05.calc_end_amount()
+        self.assertEqual(100.00, round(calc_05.return_end_amount(), 2))
 
 
     # Extention tests
