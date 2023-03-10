d832e4c20d86b1cf5a0fbd6147225bda6400a9d5
Jon Minton <jon.will.minton@gmail.com>
2023-03-10 09:44:04 +0000

    complete matrix tdd

--- wk_02/dy_4/tdd_lab/start_code/matrix/src/matrix.py
+++ wk_02/dy_4/tdd_lab/start_code/matrix/src/matrix.py
@@ -1,9 +1,28 @@
+
+
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        matrix_lines = matrix_string.split("\n")
+        matrix_lines_split = [x.split(" ") for x in matrix_lines]
+
+        nrow = len(matrix_lines)
+        ncol = len(matrix_lines_split[0]) # No checking for ragged arrays
+
+
+        outer_list = []
+        for i in range(nrow):
+            inner_list = []
+            for j in range(ncol):
+                this_num = int(matrix_lines_split[i][j])
+                inner_list.append(this_num)
+            outer_list.append(inner_list)
+
+        self.mat = outer_list
+
+        
 
     def row(self, index):
-        pass
+        return self.mat[index]
 
     def column(self, index):
-        pass
+        return [x[index] for x in self.mat]
--- wk_02/dy_4/tdd_lab/start_code/matrix/tests/matrix_test.py
+++ wk_02/dy_4/tdd_lab/start_code/matrix/tests/matrix_test.py
@@ -3,14 +3,39 @@ import unittest
 from src.matrix import Matrix
 
 class MatrixTest(unittest.TestCase):
+    def setUp(self):
+        self.sq_mat_string = "1 2\n3 4"
+        self.nonsq_mat_string = "1 2 3\n4 5 6\n7 8 9\n10 11 12"
     # Tests
 
-    def test_extract_row_from_one_number_matrix(self):
+    def test_given_one_returns_funny_one(self):
         matrix = Matrix("1")
-        self.assertEqual(matrix.row(1), [1])
+        # print(f"matrix contains: {matrix.mat}")
+        self.assertEqual([[1]], matrix.mat)
+
+    def test_given_two_elements_returns_okay(self):
+        matrix = Matrix("1 2")
+        # print(f"matrix contains {matrix.mat}")
+        self.assertEqual([[1, 2]], matrix.mat)
+
+    def test_given_four_elements_on_two_rows_returns_okay(self):
+        matrix = Matrix(self.sq_mat_string)
+        # print(f"matrix contains: {matrix.mat}")
+        self.assertEqual([[1, 2], [3, 4]], matrix.mat)
+    # def test_extract_row_from_one_number_matrix(self):
+    #     matrix = Matrix("1")
+    #     self.assertEqual(matrix.row(1), [1])
 
     # Test can extract a given row
+    def test_can_extract_rows(self):
+        matrix = Matrix(self.sq_mat_string)
+        self.assertEqual([1, 2], matrix.row(0))
+        self.assertEqual([3, 4], matrix.row(1))
 
+    def test_can_extract_columns(self):
+        matrix = Matrix(self.sq_mat_string)
+        self.assertEqual([1, 3], matrix.column(0))
+        self.assertEqual([2, 4], matrix.column(1))
     # Test can extract a given row where numbers have different number of digits
     # Example matrix:
     #
@@ -18,6 +43,9 @@ class MatrixTest(unittest.TestCase):
     # 10 20
 
     # Test can extract row from non-square matrix
+    def test_can_extract_rows_from_nonsquare(self):
+        matrix = Matrix(self.nonsq_mat_string)
+        self.assertEqual([7, 8, 9], matrix.row(2))
     # Example matrix:
     #
     # 1 2 3
