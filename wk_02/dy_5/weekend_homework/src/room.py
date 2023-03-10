e25cb8d9138c051a48642fced0e8806ae093fe99
Jon Minton <jon.will.minton@gmail.com>
2023-03-10 14:34:47 +0000

    Add functionality to check in and out guests into rooms and associated unit tests

--- wk_02/dy_5/weekend_homework/src/room.py
+++ wk_02/dy_5/weekend_homework/src/room.py
@@ -2,3 +2,18 @@ class Room:
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
+        self.songs = []
+        self.guests = []
+
+    def checkin_guest(self, guest):
+        if len(self.guests) < self.capacity:
+            self.guests.append(guest)
+    
+    def checkout_guest(self, guest):
+        self.guests.remove(guest)
+
+    def add_song_to_playlist(self, song):
+        self.songs.append(song)
+
+    def remove_song_from_playlist(self, song):
+        self.songs.remove(song)
--- wk_02/dy_5/weekend_homework/tests/room_test.py
+++ wk_02/dy_5/weekend_homework/tests/room_test.py
@@ -1,11 +1,18 @@
 import unittest
 
 from src.room import Room
+from src.guest import Guest
+from src.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room1 = Room("Blues Room", 10)
         self.room2 = Room("Reds Room", 2)
+
+        self.guest1 = Guest("Steve", 50, "Copperhead Road")
+        self.guest2 = Guest("Stevi", 200, "Dreams")
+        self.guest3 = Guest("Damen", 30, "Song 2")
+
 
     def test_rooms_have_names(self):
         self.assertEqual("Blues Room", self.room1.name)
@@ -14,3 +21,23 @@ class TestRoom(unittest.TestCase):
     def test_rooms_have_capacities(self):
         self.assertEqual(10, self.room1.capacity)
         self.assertEqual(2, self.room2.capacity)
+
+    def test_can_add_guest(self):
+        self.assertEqual(0, len(self.room1.guests))
+        self.room1.checkin_guest(self.guest3)
+        self.assertEqual(1, len(self.room1.guests))
+
+    def test_can_remove_guests(self):
+        self.room1.checkin_guest(self.guest1)
+        self.room1.checkin_guest(self.guest2)
+        self.room1.checkin_guest(self.guest3)
+        self.assertEqual(3, len(self.room1.guests))
+        self.room1.checkout_guest(self.guest2)
+        self.assertEqual(2, len(self.room1.guests))
+
+    def test_rooms_wont_admit_if_above_capacity(self):
+        self.room2.checkin_guest(self.guest1)
+        self.room2.checkin_guest(self.guest2)
+        self.assertEqual(2, len(self.room2.guests))
+        self.room2.checkin_guest(self.guest3)
+        self.assertEqual(2, len(self.room2.guests))
