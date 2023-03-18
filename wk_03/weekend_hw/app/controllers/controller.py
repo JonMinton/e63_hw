865e87b246f3ca37fbec4d226ec7ffde28843f8f
Jon Minton <jon.will.minton@gmail.com>
2023-03-18 15:21:27 +0000

    get into a delete mess

--- wk_03/weekend_hw/app/controllers/controller.py
+++ wk_03/weekend_hw/app/controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, request, redirect
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -13,6 +13,7 @@ lib_obj = Library("My library")
 lib_obj.get_library()
 all_books = lib_obj.library
 
+# all_books[13].checked_out = True
 
 @app.route('/books')
 def index():
@@ -22,9 +23,27 @@ def index():
 def root_index():
     return redirect('/books')
 
+@app.route('/books/<index>')
+def read(index):
+    return render_template('specific_book.html', this_book = all_books[int(index)], this_index = int(index))
 
-# @app.route('/orders/<index>')
-# def single_order(index):
-#   chosen_order = orders[int(index)]
-  
-#   return render_template('order.html', order=chosen_order)
+
+@app.route('/books', methods=['POST'])
+def add_book():
+  bookTitle = request.form['title']
+  newBook = Book(title=bookTitle, author = "", genre = "", checked_out = False)
+  all_books.append(newBook)
+
+  return redirect('/books')
+
+@app.route('/books', methods=['DELETE'])
+def delete_book():
+    book_title = request.form['book_name']
+    # book_to_destroy = [book for book in books if book.title == name]
+    print(f"The book's title is {book_title}")
+    # for book in all_books:
+    #     if book.title == bookTitle:
+    #         print("Found book!")
+    #         all_books.remove(book)
+
+    return redirect('/books')
