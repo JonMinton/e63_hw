a5c45ff1150688a485394e046aa43a2d74cbbcb6
Jon Minton <jon.will.minton@gmail.com>
2023-03-18 13:47:11 +0000

    start to build pages

--- wk_03/weekend_hw/app/controllers/controller.py
+++ wk_03/weekend_hw/app/controllers/controller.py
@@ -1,10 +1,27 @@
-from flask import render_template
-from app import app
-# from models.orders import orders
+from flask import render_template, redirect
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os.path
+import pickle
 
-# @app.route('/orders')
-# def index():
-#     return render_template('index.html', title='Jabba the Pizza Hutt', orders=orders)
+from models.book import Book
+from models.library import Library
+from app import app
+
+lib_obj = Library("My library")
+lib_obj.get_library()
+all_books = lib_obj.library
+
+
+@app.route('/books')
+def index():
+    return render_template('index.html', title="Jon's Occult Library", books = all_books)
+
+@app.route('/')
+def root_index():
+    return redirect('/books')
+
 
 # @app.route('/orders/<index>')
 # def single_order(index):
