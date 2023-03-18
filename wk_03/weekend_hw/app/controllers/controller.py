16fec91bf3ea85597f5233ce6d038ff71a1373cf
Jon Minton <jon.will.minton@gmail.com>
2023-03-18 20:32:20 +0000

    produce first full working version (though looks naff:) )

--- wk_03/weekend_hw/app/controllers/controller.py
+++ wk_03/weekend_hw/app/controllers/controller.py
@@ -25,7 +25,7 @@ def root_index():
 
 @app.route('/books/<index>')
 def read(index):
-    return render_template('specific_book.html', this_book = all_books[int(index)], this_index = int(index))
+    return render_template('specific_book.html', this_book = all_books[int(index)], this_index = int(index), n_books = len(all_books))
 
 
 @app.route('/books', methods=['POST'])
@@ -36,14 +36,25 @@ def add_book():
 
   return redirect('/books')
 
-@app.route('/books', methods=['DELETE'])
+@app.route('/books/delete', methods=['POST'])
 def delete_book():
-    book_title = request.form['book_name']
-    # book_to_destroy = [book for book in books if book.title == name]
-    print(f"The book's title is {book_title}")
-    # for book in all_books:
-    #     if book.title == bookTitle:
-    #         print("Found book!")
-    #         all_books.remove(book)
+    book_to_destroy = [book for book in all_books if book.title == request.form["book_name"]]
+    all_books.remove(book_to_destroy[0])
+ 
+    return redirect('/books')
+
+@app.route('/books/check_in', methods=['POST'])
+def check_in_book():
+    book_index = int(request.form["check_in"])
+    book_to_check_in = all_books[book_index]
+    book_to_check_in.checked_out = False
+
+    return redirect('/books')
+
+@app.route('/books/check_out', methods=['POST'])
+def check_out_book():
+    book_index = int(request.form["check_out"])
+    book_to_check_out = all_books[book_index]
+    book_to_check_out.checked_out = True
 
     return redirect('/books')
