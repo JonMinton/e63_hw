62b4785257b0c7dd9d3c167ddb042c6a69848fdc
Jon Minton <jon.will.minton@gmail.com>
2023-04-05 15:10:26 +0100

    complete pangram finder

--- wk_06/dy_3/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
+++ wk_06/dy_3/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,8 +1,39 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.split('').
+    map(x => x.toLowerCase()).
+    map((x) => {
+      if (x === "?") {
+        return "\\?"
+      } else {
+        return x
+      }
+    }).
+  filter(x => ("qwertyuiopasdfghjklzxcvbnm".match(x)))
+
 }
 
 PangramFinder.prototype.isPangram = function () {
+  let numMatches = 0
+  if (this.phrase.length === 0) {return false}
+  this.alphabet.forEach((x) => {
+    let matchFound = false;
+    this.phrase.forEach((y) => {
+        if (x === y) {
+          matchFound = true
+        }
+      })
+      if (matchFound) {
+        numMatches++
+      }
+    }
+  );
+
+  if (numMatches === 26) {
+    return true
+  } else {
+    return false
+  }
 
 }
 
