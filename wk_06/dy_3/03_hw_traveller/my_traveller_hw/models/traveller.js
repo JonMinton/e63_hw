777ecc666f978da3d62ba1012867802f44c850f2
Jon Minton <jon.will.minton@gmail.com>
2023-04-05 11:40:21 +0100

    complete initial js hw

--- wk_06/dy_3/03_hw_traveller/my_traveller_hw/models/traveller.js
+++ wk_06/dy_3/03_hw_traveller/my_traveller_hw/models/traveller.js
@@ -3,27 +3,35 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(x => x.startLocation)
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(x => x.endLocation)
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(x => x.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(x => x.distance >= minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, x) => total + x.distance, 0)
 
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const uniqueModes = []
+  this.journeys.forEach((x) => {
+      if (!uniqueModes.includes(x.transport)) {
+        uniqueModes.push(x.transport)
+      }
+    }
+  )
+  return uniqueModes
 };
 
 
