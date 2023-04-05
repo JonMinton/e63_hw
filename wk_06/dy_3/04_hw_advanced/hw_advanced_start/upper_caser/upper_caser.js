088565bf39b7a3c0880d6097c097644bc33f387a
Jon Minton <jon.will.minton@gmail.com>
2023-04-05 12:15:18 +0100

    do upper caser

--- wk_06/dy_3/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
+++ wk_06/dy_3/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
@@ -1,8 +1,9 @@
 const UpperCaser = function (words) {
+    this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+    return this.words.map(x => x.toUpperCase())
 }
 
 module.exports = UpperCaser;
