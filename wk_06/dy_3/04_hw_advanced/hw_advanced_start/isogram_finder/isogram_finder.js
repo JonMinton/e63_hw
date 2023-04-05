dc590cbbe88d784da8de6af9e09b8d9da9145b7f
Jon Minton <jon.will.minton@gmail.com>
2023-04-05 14:22:53 +0100

    complete isogram challenge

--- wk_06/dy_3/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
+++ wk_06/dy_3/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,28 @@
 const IsogramFinder = function (word) {
-
+    this.word = word
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const lowerWordArray = this.word.toLowerCase().split("")
+    const uniqueLetters = []   
+    console.log(lowerWordArray)
+    lowerWordArray.forEach(x => {
+        let addLetter = 0
+        uniqueLetters.forEach(y => {
+            if (y === x) {
+                addLetter++
+            }
+        })
+        if (!addLetter) {
+            uniqueLetters.push(x)
+        }
+    });
+    console.log(uniqueLetters)
+    if (lowerWordArray.length === uniqueLetters.length) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = IsogramFinder;
