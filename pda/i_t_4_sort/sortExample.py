2d56725261bd08a62800210b885ba2d7195c3329
Jon Minton <jon.will.minton@gmail.com>
2023-04-25 09:15:44 +0100

    add photo of hw diagram

---
+++ pda/i_t_4_sort/sortExample.py
@@ -0,0 +1,13 @@
+myList = [53, 8, 4, 25, 7, 90, 12]
+
+def sortList(lst):
+    return sorted(lst)
+
+
+print(f"The list before sorting: {myList}")
+
+print("A sorted version of the list will now be created")
+
+mySortedList = sortList(myList)
+
+print(f"The list after sorting: {mySortedList}")
