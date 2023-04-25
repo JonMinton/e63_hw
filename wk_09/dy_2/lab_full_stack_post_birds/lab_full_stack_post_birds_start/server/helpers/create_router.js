2fcaf426927caef5d04b88120d4292865aecb587
Jon Minton <jon.will.minton@gmail.com>
2023-04-25 17:18:45 +0100

    add working server routes

--- wk_09/dy_2/lab_full_stack_post_birds/lab_full_stack_post_birds_start/server/helpers/create_router.js
+++ wk_09/dy_2/lab_full_stack_post_birds/lab_full_stack_post_birds_start/server/helpers/create_router.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { ObjectId } = require('mongodb');
 
+const handleError = err => {
+  return err => {
+    console.error(err)
+    res.status(500) // 500: internal server error
+    res.json( { status: 500, error: err } )  
+  }
+}
+
 const createRouter = function (collection) {
 
   const router = express.Router();
 
+  // INDEX / GET ALL
   router.get('/', (req, res) => {
     collection
       .find()
@@ -17,6 +26,7 @@ const createRouter = function (collection) {
       });
   });
 
+  // GET ONE 
   router.get('/:id', (req, res) => {
     const id = req.params.id;
     collection
@@ -28,6 +38,50 @@ const createRouter = function (collection) {
         res.json({ status: 500, error: err });
       });
   });
+
+  // DELETE ONE 
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne({ _id: new ObjectId(id) })
+    .then(result => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  // ADD NEW 
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+    .insertOne(newData)
+    .then((result) => {
+      res.json( { _id: result.insertedId })
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  // UPDATE by ID 
+  router.put("/:id", (req, res) => {
+    const id = req.params.id; 
+    const updatedData = req.body
+    collection
+      .updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      )
+      .then(result => res.json(result))
+      .catch(handleError(res))
+  })
+
 
   return router;
 };
